e8d8d35d09ef8f5b6d8049c3770302b76047904b
ggiann02 <giorgianna26@gmail.com>
2025-07-23 10:35:21 -0400

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-green-100 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-green-800">Tailwind is Working ✅</h1>
+    </div>
+  );
+}
